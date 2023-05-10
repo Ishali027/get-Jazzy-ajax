41f0875f6d9a1597d5ce2585fe48e8b0ef052acb
Ismail Ali <ishaliprime@gmail.com>
2023-05-10 13:59:11 -0500

    finished the assignment and was able to make 1 of the stretch problems work

--- server/public/client.js
+++ server/public/client.js
@@ -1,6 +1,11 @@
 $(document).ready(onReady);
 
 function onReady() {
+    // this function is letting us pull the information of artists 
+    // and letting us manipulate it
+    // we then make a function to loop through the array  
+    // we then append table data into the table body and show it 
+    //on the DOM
     $.ajax({
         type: 'GET',
         url: '/artist'
@@ -18,4 +23,22 @@ function onReady() {
     });
 
     // TODO Add ajax request for /songs and display on DOM
+    $.ajax({
+        type: 'GET', 
+        url:  '/songs'
+    }).then(function (response){
+        for (let i = 0; i<response.length; i++){
+            let song = response[i];
+            $('#songTableBody').append(`
+            <tr>
+            <td>${song.title}</td>
+            <td>${song.artist}</td>
+        </tr>
+            `)
+        }
+    })
+
+
+
+
 }
--- server/server.js
+++ server/server.js
@@ -3,47 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-const artistListArray = [
-    {
-        name: 'Miles Davis',
-        born: 1926,
-        died: 1990,
-    },
-    {
-        name: 'Duke Ellington',
-        born: 1899,
-        died: 1974,
-    },
-    {
-        name: 'John Coltrane',
-        born: 1926,
-        died: 1987,
-    },
-    {
-        name: 'Louis Daniel Armstrong',
-        born: 1901,
-        died: 1971,
-    },
-];
-
-const songListArray = [
-    {
-        title: 'Take Five',
-        artist: 'The Dave Brubeck Quartet',
-    },
-    {
-        title: 'So What',
-        artist: 'Miles Davis',
-    },
-    {
-        title: 'Sing Sing Sing',
-        artist: 'Benny Goodman',
-    },
-    {
-        title: 'Take the "A" Train',
-        artist: 'The Dave Brubeck Quartet',
-    },
-];
+const artistListArray = require('./Modules/artist')
+    
+const songListArray = require('./Modules/song')
 
 app.use(express.static('server/public'));
 
@@ -52,6 +14,15 @@ app.get('/artist', (req, res) => {
 });
 
 // TODO - Add GET for songs
+app.get('/songs', (req, res) => {
+    res.send(songListArray)
+});
+
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
